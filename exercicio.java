5a24a576027be1ac73d272f542ab9278eeb9c03c
Labelle Cândido <126924842+LABELLECANDIDO@users.noreply.github.com>
2023-03-24 20:42:28 -0300

    exercicio - neutro/positivo?negativo
    Escreva um programa para receber um número e mostrar se o número é positivo ou negativo.

---
+++ exercicio.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	
+	double numero;
+	
+	    System.out.println("Informe um numero: ");
+	numero = input.nextDouble();
+	
+	if (numero>0){
+	    System.out.println("NUMERO POSITIVO.");
+    }
+	else if(numero<0){
+	    System.out.println("NUMERO NEGATIVO");
+	}
+	else {
+        System.out.println("O número é zero.");
+        }
+	input.close();
+	}
+}
+
