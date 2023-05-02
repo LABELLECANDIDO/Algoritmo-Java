08c448e8da9f476592c91053760b36cb26eec89e
Labelle Cândido <126924842+LABELLECANDIDO@users.noreply.github.com>
2023-05-02 09:20:04 -0300

    vetores

---
+++ vetores.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		
+		int n [] = {3, 2, 8, 7, 5, 4};
+		
+		 for  (int c=0; c<=5; c++){
+		     System.out.println("na posição " + c + " temos o valor " + n[c]);
+		 }
+	}
+}
