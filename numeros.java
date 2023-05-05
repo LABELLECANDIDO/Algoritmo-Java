e06e888856d905aa5367adc30583505614b68ea8
Labelle Cândido <126924842+LABELLECANDIDO@users.noreply.github.com>
2023-05-05 20:58:31 -0300

    mostrar todos os números de 10 a 20.
    um programa para mostrar todos os números de 10 a 20 - estrutura de repetição

--- numeros.java
+++ numeros.java
@@ -1,24 +1,16 @@
-
 import java.util.Scanner;
 public class Main
 {
-	public static void main(String[] args) {
-	 Scanner input = new Scanner(System.in);
-	 
-	 int primeiroNumero = 5;
-	  int segundoNumero = 10;
-	  
-	  if (primeiroNumero>=segundoNumero){
-	      primeiroNumero = segundoNumero;
-	      segundoNumero = primeiroNumero;
-	  }
-	  else {
-	      segundoNumero = primeiroNumero;
-	      primeiroNumero = segundoNumero;
-	  }
-	 System.out.println("primeiro numero: " + primeiroNumero);
-	 System.out.println("segundo numero: " + segundoNumero);
-	 
-	 input.close();
-	}
+  public static void main (String[]args)
+  {
+    Scanner input = new Scanner (System.in);
+
+    int numero = 10;
+    while (numero <= 20)
+      {
+	System.out.printf("%d\n", numero);
+	numero = numero + 1;
+      }
+       input.close();
+  }
 }
